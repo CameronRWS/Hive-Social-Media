438b8cf98c874ee08101e5092a560e5803b92bb9
Catherine Larson <clarson1@iastate.edu>
2020-09-01 18:34:35 -0500

    switch from activities to fragments

--- Frontend/hiveFrontend/app/src/main/java/com/example/navigationexperiment/MainActivity.java
+++ Frontend/hiveFrontend/app/src/main/java/com/example/navigationexperiment/MainActivity.java
@@ -22,22 +22,13 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications, R.id.navigation_buzz, R.id.navigation_profile)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
     }
 
-    public void makePost(View view){
-        Intent intent = new Intent(this, MakePostActivity.class);
-        startActivity(intent);
-    }
-
-    public void viewProfile(View view){
-        Intent intent = new Intent(this, ProfileActivity.class);
-        startActivity(intent);
-    }
 
     public void viewSettings(View view){
         Intent intent = new Intent(this, SettingsActivity.class);
---
+++ Frontend/hiveFrontend/app/src/main/java/com/example/navigationexperiment/ui/buzz/BuzzViewModel.java
@@ -0,0 +1,7 @@
+package com.example.navigationexperiment.ui.buzz;
+
+import androidx.lifecycle.ViewModel;
+
+public class BuzzViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
