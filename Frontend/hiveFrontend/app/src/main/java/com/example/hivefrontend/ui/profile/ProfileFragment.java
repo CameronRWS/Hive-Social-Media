ee3cb526f7b75e3e03a95567c56ef01f4f41dc94
Catherine Larson <clarson1@iastate.edu>
2020-09-08 15:15:31 -0500

    inital attempt with android volley

--- Frontend/hiveFrontend/app/src/main/java/com/example/hivefrontend/ui/profile/ProfileFragment.java
+++ Frontend/hiveFrontend/app/src/main/java/com/example/hivefrontend/ui/profile/ProfileFragment.java
@@ -11,8 +11,25 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.hivefrontend.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
 
 public class ProfileFragment extends Fragment {
 
@@ -25,7 +42,59 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.profile_fragment, container, false);
+
+        View rootView = inflater.inflate(R.layout.profile_fragment, container, false);
+
+
+        final TextView textView = (TextView) rootView.findViewById(R.id.text);
+
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(getActivity().getApplicationContext());
+        String url ="coms-309-tc-03.cs.iastate.edu:8080/users";
+
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+
+                    @Override
+                    public void onResponse(JSONArray response) {
+
+
+                        try{
+
+                            JSONObject user1 = response.getJSONObject(1);
+
+                            // Get the current user (json object) data
+                            String name = user1.getString("displayName");
+
+
+                            // Display the formatted json data in text view
+                            textView.setText(name);
+
+                        }
+                        catch (JSONException e){
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+                        VolleyLog.d("Error: " + error.getMessage());
+                        textView.setText("That didn't work :( ");
+
+                    }
+                });
+
+
+
+// Add the request to the RequestQueue.
+        queue.add(jsonArrayRequest);
+
+        return rootView;
     }
 
     @Override
@@ -33,6 +102,10 @@ public class ProfileFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
         mViewModel = ViewModelProviders.of(this).get(ProfileViewModel.class);
         // TODO: Use the ViewModel
+
+
     }
+
+
 
 }
