ec5f3e292f933cda88984c1a2c73ec362393d453
Julie Van Kirk <jvankirk@iastate.edu>
2020-09-09 22:47:38 -0500

    add RequestIdentity object

--- Backend/hive/src/main/java/hive/app/request/RequestIdentity.java
+++ Backend/hive/src/main/java/hive/app/request/RequestIdentity.java
@@ -1,5 +1,44 @@
 package hive.app.request;
 
-public class RequestIdentity {
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import hive.app.user.User;
 
+@Embeddable
+public class RequestIdentity {
+	
+	private static final long serializedVersionUID = 1L;
+	
+	@Column (name = "hive_id")
+	private int hiveId;
+	
+	@ManyToOne
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
+	
+	public RequestIdentity() {};
+	
+	public RequestIdentity(int hiveId, User user) {
+		this.hiveId = hiveId;
+		this.user = user;
+	}
+	
+	public int getHiveId() {
+		return hiveId;
+	}
+	
+	public User getUser() {
+		return user;
+	}
+	
+	public void setHiveId(int hiveId) {
+		this.hiveId = hiveId;
+	}
+	
+	public void setUser(User user) {
+		this.user = user;
+	}
 }
