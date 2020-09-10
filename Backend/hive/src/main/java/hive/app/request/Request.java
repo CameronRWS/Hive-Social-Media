d632a10ca597531e63714215913e080e7a111321
Julie Van Kirk <jvankirk@iastate.edu>
2020-09-09 22:36:47 -0500

    add Request model object

--- Backend/hive/src/main/java/hive/app/request/Request.java
+++ Backend/hive/src/main/java/hive/app/request/Request.java
@@ -1,5 +1,40 @@
 package hive.app.request;
 
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import hive.app.user.User;
+import hive.app.utils.DateTime;
+
+@Entity
+@Table(name = "requests")
 public class Request {
 
+	// for now, generate key as normal. want embedded class after RequestIdentity is created
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "comment_id")
+	private int requestIdentity;
+	
+	@Column (name = "date_created")
+	private String dateCreated;
+	
+	//public Request() {}
+	
+	public Request(){
+		this.dateCreated = DateTime.GetCurrentDateTime();
+	}
+	
+	public String getDateCreated() {
+		return dateCreated;
+	}
+	
+	public void setDateCreated(String dateCreated) {
+		this.dateCreated = dateCreated;
+	}
 }
+
