06c460ecc80893ea64a46d59ff16238be17e8a88
Julie Van Kirk <jvankirk@iastate.edu>
2020-09-10 21:55:32 -0500

    add RequestRepository

--- Backend/hive/src/main/java/hive/app/request/RequestRepository.java
+++ Backend/hive/src/main/java/hive/app/request/RequestRepository.java
@@ -1,5 +1,13 @@
 package hive.app.request;
 
-public class RequestRepository {
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public interface RequestRepository extends JpaRepository<Request, RequestIdentity> {
+	@Query("SELECT l FROM Request l WHERE l.requestIdentity.hiveId = :hiveId")
+	List<Request> findByHiveId(@Param("hiveId") int hiveId);
 }
