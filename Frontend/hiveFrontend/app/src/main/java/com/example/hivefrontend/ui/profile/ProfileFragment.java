b9b78ecbac3d546df32e538083ecee26a6f4a929
Young Ha <youngha@iastate.edu>
2020-09-20 19:32:52 -0500

    Location text box now gets user's real location and the text and pin will be hidden if there is no provided location.

--- Frontend/hiveFrontend/app/src/main/java/com/example/hivefrontend/ui/profile/ProfileFragment.java
+++ Frontend/hiveFrontend/app/src/main/java/com/example/hivefrontend/ui/profile/ProfileFragment.java
@@ -7,14 +7,11 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -30,16 +27,9 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.ArrayList;
-
 public class ProfileFragment extends Fragment {
 
-    //private RecyclerView recyclerView;
-    //private RecyclerView.Adapter mAdapter;
-    //private RecyclerView.LayoutManager layoutManager;
     private ProfileViewModel mViewModel;
-    private ArrayList<Integer> hiveIds;
-    private ArrayList<String> hiveOptions;
 
     public static ProfileFragment newInstance() {
         return new ProfileFragment();
@@ -49,14 +39,17 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
 
-        final View rootView = inflater.inflate(R.layout.profile_fragment, container, false);
-        hiveIds= new ArrayList<>();
-        hiveOptions = new ArrayList<>();
+        View rootView = inflater.inflate(R.layout.profile_fragment, container, false);
+
         final TextView textView = (TextView) rootView.findViewById(R.id.displayName);
+        final ImageView locationPin = (ImageView) rootView.findViewById(R.id.locationPin);
         final TextView userName = (TextView) rootView.findViewById(R.id.userName);
+        final TextView pollenCount = (TextView) rootView.findViewById(R.id.pollenCount);
+        final TextView displayLocation = (TextView) rootView.findViewById(R.id.displayLocation);
         final TextView hiveListHeading = (TextView) rootView.findViewById(R.id.hiveListHeading);
         final TextView bio = (TextView) rootView.findViewById(R.id.bio);
-        final TextView hiveList = (TextView) rootView.findViewById(R.id.hiveList);
+        final TextView dateJoined = (TextView) rootView.findViewById(R.id.dateJoined);
+
 
         // Instantiate the RequestQueue.
         RequestQueue queue = Volley.newRequestQueue(getActivity().getApplicationContext());
@@ -64,14 +57,14 @@ public class ProfileFragment extends Fragment {
 
         // Server name http://coms-309-tc-03.cs.iastate.edu:8080/posts
 
-        //first request: user information
+
         JsonArrayRequest jsonArrayRequest = new JsonArrayRequest
                 (Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
 
                     @Override
                     public void onResponse(JSONArray response) {
                         try{
-                            JSONObject user1 = response.getJSONObject(4);
+                            JSONObject user1 = response.getJSONObject(3);
                             // Get the current user (json object) data
                             String name = user1.getString("displayName");
 
@@ -82,76 +75,32 @@ public class ProfileFragment extends Fragment {
                             }
 
                             String uName = user1.getString("userName");
+                            String location = user1.getString("location");
                             // Formats username and concatenates an '@' before.
                             uName = uName.toLowerCase();
                             uName = "@" + uName;
                             // Display the formatted json data in text view
                             textView.setText(name);
+                            displayLocation.setText(location);
+
+                            // get rid of the location pin if the location is null
+                            if (location == "null")
+                            {
+                                displayLocation.setVisibility(View.INVISIBLE);
+                                locationPin.setVisibility(View.INVISIBLE);
+                            }
+
                             userName.setText(uName);
                             bio.setText(user1.getString("biography"));
                             // TODO: replace '(4)' with actual count.
                             if (firstName.length() == 0)
                             {
-                                hiveListHeading.setText("Their Public Hives:");
+                                hiveListHeading.setText("Their Public Hives (4)");
                             }
                             else {
-                                hiveListHeading.setText((firstName + "'s Public Hives:"));
+                                hiveListHeading.setText((firstName + "'s Public Hives (4)"));
                             }
                             //dateJoined.setText(user1.getString("dateCreated"));
-                        }
-                        catch (JSONException e){
-                            e.printStackTrace();
-                            Log.i("jsonAppError",e.toString());
-                        }
-
-                    }
-                }, new Response.ErrorListener() {
-
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        // TODO: Handle error
-                        Log.i("volleyAppError","Error: " + error.getMessage());
-                        Log.i("volleyAppError","VolleyError: "+ error);
-
-                        textView.setText("Error.");
-
-                    }
-                });
-        //second request: hive information
-        url ="http://10.24.227.37:8080/members/byUserId/1"; //for now, getting this user's hive information until we have login functionality
-
-        JsonArrayRequest hiveRequest = new JsonArrayRequest
-                (Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
-
-                    @Override
-                    public void onResponse(JSONArray response) {
-                        try{
-                            for(int i = 0; i < response.length(); i++){
-                                JSONObject member = response.getJSONObject(i); //should return user,hive pair
-                                Integer hiveId = (Integer) member.getJSONObject("hive").getInt("hiveId");
-                                hiveIds.add(hiveId);
-                                String hiveName =  member.getJSONObject("hive").getString("name");
-                                hiveOptions.add(hiveName);
-                            }
-                            //here the hives' ids and names have been set appropriately
-                            String hiveString = "";
-                            for(int i = 0; i<hiveOptions.size(); i++){
-                                if(i==hiveOptions.size()-1){
-                                    hiveString = hiveString + "" + hiveOptions.get(i);
-                                }
-                                else{
-                                    hiveString = hiveString + "" + hiveOptions.get(i) + ", ";
-                                }
-                            }
-                            hiveList.setText(hiveString);
-
-                            // set up the RecyclerView
-                           // RecyclerView recyclerView = rootView.findViewById(R.id.hiveList);
-                            //recyclerView.setLayoutManager(new LinearLayoutManager(getActivity().getApplicationContext()));
-                            //mAdapter = new MyAdapter(getActivity().getApplicationContext(), hiveOptions);
-                            //recyclerView.setAdapter(mAdapter);
-                            //mAdapter.notifyDataSetChanged();
-
                         }
                         catch (JSONException e){
                             e.printStackTrace();
@@ -176,12 +125,9 @@ public class ProfileFragment extends Fragment {
 
 // Add the request to the RequestQueue.
         queue.add(jsonArrayRequest);
-        queue.add(hiveRequest);
 
         return rootView;
     }
-
-
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
