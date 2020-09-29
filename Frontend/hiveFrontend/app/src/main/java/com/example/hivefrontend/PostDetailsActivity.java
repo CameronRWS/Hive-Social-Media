0432ce97ded828ea5b59af660ba2f6d626dc7dcf
Catherine Larson <clarson1@iastate.edu>
2020-09-29 14:36:37 -0500

    displaying post information

--- Frontend/hiveFrontend/app/src/main/java/com/example/hivefrontend/PostDetailsActivity.java
+++ Frontend/hiveFrontend/app/src/main/java/com/example/hivefrontend/PostDetailsActivity.java
@@ -20,6 +20,7 @@ public class PostDetailsActivity extends AppCompatActivity {
 
 
     private RequestQueue queue;
+    private String hiveName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,13 +32,13 @@ public class PostDetailsActivity extends AppCompatActivity {
         titleTextView.setText(""+postId);
         queue = Volley.newRequestQueue(this);
 
-        String url ="http://10.24.227.37:8080/posts/byPostId/" + postId; //for now, getting this user's hive information until we have login functionality
+        String url ="http://10.24.227.37:8080/posts/byPostId/" + postId;
         JsonObjectRequest postDetailsRequest = new JsonObjectRequest
                 (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
                         try {
-                            setPostInfo(response);
+                            getHiveName(response);
                         }
                         catch (JSONException e) {
                             e.printStackTrace();
@@ -54,9 +55,62 @@ public class PostDetailsActivity extends AppCompatActivity {
         queue.add(postDetailsRequest);
     }
 
+    private void getHiveName(final JSONObject post) throws JSONException {
+        int hiveId = post.getInt("hiveId");
+
+        String url ="http://10.24.227.37:8080/hives/byHiveId/" + hiveId;
+        JsonObjectRequest hiveNameRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            hiveName = response.getString("name");
+                            setPostInfo(post);
+                        }
+                        catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+                        Log.i("volleyAppError","Error: " + error.getMessage());
+                        Log.i("volleyAppError","VolleyError: "+ error);
+                    }
+                });
+        queue.add(hiveNameRequest);
+
+
+    }
+
     public void setPostInfo(JSONObject post) throws JSONException {
+        //title
         TextView titleText = this.findViewById(R.id.postTitle);
         String title = post.getString("title");
         titleText.setText(title);
+        //content
+        TextView postContent = this.findViewById(R.id.postContent);
+        String content = post.getString("textContent");
+        postContent.setText(content);
+        //user display name
+        TextView displayName = this.findViewById(R.id.userDisplayName);
+        String name = post.getJSONObject("user").getString("displayName");
+        displayName.setText(name);
+        //user name
+        TextView userNameTextView = this.findViewById(R.id.userName);
+        String userName = post.getJSONObject("user").getString("userName");
+        userNameTextView.setText(userName);
+        //hive name
+        TextView hive = this.findViewById(R.id.hiveName);
+        hive.setText(hiveName);
+        //number likes
+        TextView numLikes = this.findViewById(R.id.likeNumber);
+        String likes = String.valueOf(post.getJSONArray("likes").length())+" likes";
+        numLikes.setText(likes);
+        //number comments
+        TextView numComments = this.findViewById(R.id.commentNumber);
+        String comments = String.valueOf(post.getJSONArray("comments").length())+" comments";
+        numComments.setText(comments);
     }
 }
