a484798b862a2b9472b3b5c6a46571d51cbb4df8
Young Ha <youngha@iastate.edu>
2020-10-02 17:03:09 -0500

    Test

--- Frontend/hiveFrontend/app/src/main/java/com/example/hivefrontend/LoginActivity.java
+++ Frontend/hiveFrontend/app/src/main/java/com/example/hivefrontend/LoginActivity.java
@@ -18,7 +18,7 @@ public class LoginActivity extends AppCompatActivity {
         signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                 startActivity(intent);
             }
         });
