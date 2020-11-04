154331360db604755b19ca430d8f887d5bdd9b7c
Young Ha <youngha@iastate.edu>
2020-11-04 16:09:27 -0600

    test

--- Frontend/hiveFrontend/app/src/test/java/com/example/hivefrontend/ExampleUnitTest.java
+++ Frontend/hiveFrontend/app/src/test/java/com/example/hivefrontend/ExampleUnitTest.java
@@ -1,6 +1,38 @@
 package com.example.hivefrontend;
 
 import org.junit.Test;
+import com.example.hivefrontend.PostDetails.PostDetailsActivity;
+import com.example.hivefrontend.Profile.ProfileActivity;
+import com.example.hivefrontend.ui.home.HomeFragment;
+import com.example.hivefrontend.PostDetails.Logic.PostDetailsLogic;
+import com.example.hivefrontend.PostDetails.Network.ServerRequest;
+
+import com.example.hivefrontend.PostDetails.PostDetailsActivity;
+import com.example.hivefrontend.Profile.ProfileActivity;
+import com.example.hivefrontend.ui.home.HomeFragment;
+import com.example.hivefrontend.PostDetails.Logic.PostDetailsLogic;
+import com.example.hivefrontend.PostDetails.Network.ServerRequest;
+
+import org.json.JSONObject;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyObject;
+import static org.mockito.ArgumentMatchers.isA;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.internal.verification.VerificationModeFactory.times;
+
 
 import static org.junit.Assert.*;
 
