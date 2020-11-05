54e4233a302cc69e05c3b13898a5d66b80e9fe61
Julie Van Kirk <jvankirk@iastate.edu>
2020-11-04 19:31:27 -0600

    test create member

--- Backend/hive/src/test/java/hive/app/memberTests/MemberServiceTest.java
+++ Backend/hive/src/test/java/hive/app/memberTests/MemberServiceTest.java
@@ -25,6 +25,7 @@ import hive.app.hive.HiveRepository;
 import hive.app.member.Member;
 import hive.app.member.MemberIdentity;
 import hive.app.member.MemberRepository;
+import hive.app.member.MemberService;
 import hive.app.notification.Notification;
 import hive.app.notification.NotificationRepository;
 import hive.app.request.Request;
@@ -40,6 +41,85 @@ public class MemberServiceTest {
 	@TestConfiguration
 	static class test {
 		
+		@Bean
+		public MemberService sService() {
+			return new MemberService();
+		}
+		
+		@Bean
+		public MemberRepository mRepository() {
+			return mock(MemberRepository.class);
+		}
+		
+		@Bean
+		public UserRepository uRepository() {
+			return mock(UserRepository.class);
+		}
+		
+		@Bean
+		public HiveRepository hRepository() {
+			return mock(HiveRepository.class);
+		}
+		
+		@Bean
+		public NotificationRepository nRepository() {
+			return mock(NotificationRepository.class);
+		}
 	}
 
+	@Autowired
+	private MemberService ms;
+	
+	@Autowired
+	private MemberRepository mr;
+	
+	@Autowired
+	private UserRepository ur;
+	
+	@Autowired
+	private HiveRepository hr;
+	
+	@Autowired
+	private NotificationRepository nr;
+	
+	public Hive testHive;
+	public User testUser;
+	public HashMap<String, String> body;
+	
+	@Before
+	public void beforeTests() {
+		when(mr.save((Member)any(Member.class))).thenAnswer(new Answer<Member>() {
+		    public Member answer(InvocationOnMock invocation) throws Throwable {
+		    	Object[] args = invocation.getArguments();
+		    	return (Member) args[0];
+		    }
+		});
+	}
+	
+	@Test
+	public void testCreate() {
+		testHive = new Hive("name", "description", "type", Double.valueOf("10"), Double.valueOf("20"));
+		testUser = new User("Casper", "displayName", "birthday", "biography", "location");
+		
+		when(hr.findOne((Integer) any(Integer.class))).thenReturn(this.testHive);
+		when(ur.findOne((Integer) any(Integer.class))).thenReturn(this.testUser);
+	
+		body = new HashMap<String, String>();
+		body.put("hiveId", "1");
+		body.put("userId", "2");
+		
+		ms.create(body);
+		
+	// check for created member
+		verify(mr, times(1)).save((Member) any(Member.class));
+		
+		resetMocked();
+	}
+	
+	public void resetMocked() {
+		reset(mr);
+		reset(ur);
+		reset(hr);
+		reset(nr);
+	}
 }
