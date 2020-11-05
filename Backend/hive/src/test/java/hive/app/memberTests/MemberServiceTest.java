2061eecbd6912c0cf0013f09819fb66dbaf9da37
Julie Van Kirk <jvankirk@iastate.edu>
2020-11-04 18:13:20 -0600

    initial setup

---
+++ Backend/hive/src/test/java/hive/app/memberTests/MemberServiceTest.java
@@ -0,0 +1,40 @@
+package hive.app.memberTests;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.junit4.SpringRunner;
+import hive.app.hive.Hive;
+import hive.app.hive.HiveRepository;
+import hive.app.member.Member;
+import hive.app.member.MemberIdentity;
+import hive.app.member.MemberRepository;
+import hive.app.notification.Notification;
+import hive.app.notification.NotificationRepository;
+import hive.app.request.Request;
+import hive.app.request.RequestIdentity;
+import hive.app.request.RequestRepository;
+import hive.app.request.RequestService;
+import hive.app.user.User;
+import hive.app.user.UserRepository;
+
+@RunWith(SpringRunner.class)
+public class MemberServiceTest {
+
+}
