dc2f3e43329774d349d6a01526500b5e5b1925c6
Y.J. Ha <yjha@YJs-MacBook-Pro.local>
2020-11-15 01:11:39 -0600

    two Y.J. tests.

---
+++ Frontend/hiveFrontend/app/src/test/java/com/example/hivefrontend/RegisterRoboelectricTest.java
@@ -0,0 +1,58 @@
+package com.example.hivefrontend;
+
+import android.widget.EditText;
+
+import com.example.hivefrontend.Login.LoginActivity;
+import com.example.hivefrontend.Register.RegisterActivity;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(sdk = 28)
+public class RegisterRoboelectricTest {
+
+
+    @Mock
+    private RegisterActivity registerActivity;
+
+    @Before
+
+
+    public void setUp() throws Exception
+    {
+        
+        registerActivity = Robolectric.buildActivity( RegisterActivity.class )
+                .create()
+                .resume()
+                .get();
+
+    }
+
+    @Test
+    public void shouldNotBeNull() throws Exception
+    {
+        assertNotNull( registerActivity );
+    }
+
+    @Test
+    public void emailAddressValidity() {
+        EditText emailAddress = registerActivity.findViewById(R.id.confirmEmailAddressField);
+        emailAddress.setText("demo4@gmail.com");
+
+        String emailStringLiteral = emailAddress.getText().toString();
+
+        assertEquals(emailStringLiteral,"demo4@gmail.com");
+    }
+
+
+
+}
